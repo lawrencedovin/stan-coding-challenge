a9a8706922fc589c34aa9abdf2845b5bd3d55f6a
Lawrence Dovin <lawrencedovin@gmail.com>
2021-09-29 15:07:34 -0400

    Updated series to output from sample data

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,28 +7,24 @@ const Home = () => (
   <div className={styles.Home} data-testid="Home">
     <div class="container">
       <div class="row justify-content-md-center">
-        <div class="col-sm">
-          <div className={styles.card}>
-            <img class="card-img-top" src={placeholder} alt="Card image cap" />
-            <div class="card-body">
-              <h5 class="card-title">SERIES</h5>
-              <p class="card-text">Check out some of our Series!</p>
-            </div>
-            <div class="card-body">
-              <a href="#" class="card-link">Check Series</a>
-            </div>
+        <div className={styles.card}>
+          <img class="card-img-top" src={placeholder} alt="Series Card" />
+          <div class="card-body">
+            <h5 class="card-title">SERIES</h5>
+            <p class="card-text">Check out some of our Series!</p>
+          </div>
+          <div class="card-body">
+            <a href="/series" class="card-link">Check Series</a>
           </div>
         </div>
-        <div class="col-sm">
-          <div className={styles.card}>
-            <img class="card-img-top" src={placeholder} alt="Card image cap" />
-            <div class="card-body">
-              <h5 class="card-title">MOVIES</h5>
-              <p class="card-text">Check out some of our Movies!</p>
-            </div>
-            <div class="card-body">
-              <a href="#" class="card-link">Check Movies</a>
-            </div>
+        <div className={styles.card}>
+          <img class="card-img-top" src={placeholder} alt="Movies Card" />
+          <div class="card-body">
+            <h5 class="card-title">MOVIES</h5>
+            <p class="card-text">Check out some of our Movies!</p>
+          </div>
+          <div class="card-body">
+            <a href="series" class="card-link">Check Movies</a>
           </div>
         </div>
       </div>
--- src/components/Series/Series.js
+++ src/components/Series/Series.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './Series.module.scss';
+import data from "../../feed/sample.json";
 
 const Series = () => (
   <div className={styles.Series} data-testid="Series">
-    Series Component
+    
+    {Object.keys(data.entries).map((item, i) => (
+      <ul>
+        <li>{data.entries[item].title}</li>
+      </ul>
+    ))}
+
   </div>
 );
 
