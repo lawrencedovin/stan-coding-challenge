e9d3798970bb74b3eb86e3c47cfd2e39ef68bd76
Lawrence Dovin <lawrencedovin@gmail.com>
2021-09-29 16:01:50 -0400

    Added initial conditional for filtering series

--- src/components/Series/Series.js
+++ src/components/Series/Series.js
@@ -1,32 +1,42 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styles from './Series.module.scss';
 import data from "../../feed/sample.json";
 
-const Series = () => (
-  <div className={styles.Series} data-testid="Series">
-    <div class="container">
-      <div class="row justify-content-md-center">
-        {Object.keys(data.entries).map((item, i) => (
-          <div className={styles.card}>
-            <img class="card-img-top" src={data.entries[item].images["Poster Art"].url} alt="Series Card" />
-            <div class="card-body">
-              <h5 class="card-title">SERIES</h5>
-              <p class="card-text">Check out some of our Series!</p>
+function Series() {
+  // let series = data.entries.map((item) => data.entries[item]).filter((item) => item.title === "Wolf Creek" ? item : "")
+  return (
+    <div className={styles.Series} data-testid="Series">
+      <div class="container">
+        <div class="row justify-content-md-center">
+          {data.entries.map((item) => (
+            item.title === "Wolf Creek" 
+            ?
+            <div className={styles.card}>
+              <img class="card-img-top" src={item.images["Poster Art"].url} alt="Series Card" />
+              <p>{item.title}</p>
             </div>
-            <div class="card-body">
-              <a href="/series" class="card-link">Check Series</a>
+            :
+            <div></div>
+          ))}
+          {/* {Object.keys(data.entries).map((item) => (
+            <div className={styles.card}>
+              <img class="card-img-top" src={data.entries[item].images["Poster Art"].url} alt="Series Card" />
+              <p>{data.entries[item].title}</p>
             </div>
-          </div>
-        ))}
+          )).filter((item) => data.entries[item].title === 'Wolf Creek')} */}
+          {/* {Object.keys(data.entries).map((item) => (
+            <div className={styles.card}>
+              <img class="card-img-top" src={data.entries[item].images["Poster Art"].url} alt="Series Card" />
+              <p>{data.entries[item].title}</p>
+            </div>
+          )).filter((item) => item)} */}
+          {/* {data.entries.map((item) => (
+            <p>{item.title === "Wolf Creek" ? item.title : ""}</p>
+          ))} */}
+        </div>
       </div>
     </div>
-
-  </div>
-);
-
-Series.propTypes = {};
-
-Series.defaultProps = {};
+  )
+}
 
 export default Series;
