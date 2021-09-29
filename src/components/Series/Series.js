68d7068c59c2917f868c9b9327f772f999fc30d3
Lawrence Dovin <lawrencedovin@gmail.com>
2021-09-29 15:13:26 -0400

    added initial card for series

--- src/components/Series/Series.js
+++ src/components/Series/Series.js
@@ -5,12 +5,22 @@ import data from "../../feed/sample.json";
 
 const Series = () => (
   <div className={styles.Series} data-testid="Series">
-    
-    {Object.keys(data.entries).map((item, i) => (
-      <ul>
-        <li>{data.entries[item].title}</li>
-      </ul>
-    ))}
+    <div class="container">
+      <div class="row justify-content-md-center">
+        {Object.keys(data.entries).map((item, i) => (
+          <div className={styles.card}>
+            <img class="card-img-top" src={data.entries[item].images["Poster Art"].url} alt="Series Card" />
+            <div class="card-body">
+              <h5 class="card-title">SERIES</h5>
+              <p class="card-text">Check out some of our Series!</p>
+            </div>
+            <div class="card-body">
+              <a href="/series" class="card-link">Check Series</a>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
 
   </div>
 );
