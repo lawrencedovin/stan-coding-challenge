ca6416afc57ddd79222f63de8d10b609a19d5f82
Lawrence Dovin <lawrencedovin@gmail.com>
2021-09-29 17:45:31 -0400

    Returns only objects that are in the series Title

--- src/components/Series/Series.js
+++ src/components/Series/Series.js
@@ -14,20 +14,27 @@ function Series() {
     null
   ))
   seriesTitle = seriesTitle.sort().slice(0, 21);
+  data.entries.map((item) => (
+    seriesTitle.includes(item.title)
+    ?
+    series.push(item)
+    :
+    null 
+  ))
   // Extract
   // Order by item.title in series
   return (
     <div className={styles.Series} data-testid="Series">
       <div class="container">
-        {/* <div class="row justify-content-md-center">
+        <div class="row justify-content-md-center">
           {series.slice(0, 21).map((item, i) => (
             <div className={styles.card}>
               <img class="card-img-top" src={item.images["Poster Art"].url} alt="Series Card" />
               <p>{item.title}</p>
             </div>
           ))}
-        </div> */}
-        <div>{seriesTitle.map((title) => title)}</div>
+        </div>
+        {/* <div>{seriesTitle.map((title) => title)}</div> */}
       </div>
     </div>
   )
