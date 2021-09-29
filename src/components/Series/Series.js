6a45eede7ff1f552c3576ed2f5901e2249652060
Lawrence Dovin <lawrencedovin@gmail.com>
2021-09-29 16:18:48 -0400

    Refactored code

--- src/components/Series/Series.js
+++ src/components/Series/Series.js
@@ -3,20 +3,23 @@ import styles from './Series.module.scss';
 import data from "../../feed/sample.json";
 
 function Series() {
-  // let series = data.entries.map((item) => data.entries[item]).filter((item) => item.title === "Wolf Creek" ? item : "")
+  let series = []
+  data.entries.map((item) => (
+    item.releaseYear >= 2010 && item.programType === 'series'
+    ?
+    series.push(item)
+    :
+    null
+  ))
   return (
     <div className={styles.Series} data-testid="Series">
       <div class="container">
         <div class="row justify-content-md-center">
-          {data.entries.map((item, i) => (
-            i < 21
-            ?
+          {series.map((item, i) => (
             <div className={styles.card}>
               <img class="card-img-top" src={item.images["Poster Art"].url} alt="Series Card" />
               <p>{item.title}</p>
             </div>
-            :
-            <></>
           ))}
         </div>
       </div>
