b7e5f4838b2b95f69ff6aac745c914c81dffd155
Lawrence Dovin <lawrencedovin@gmail.com>
2021-09-29 17:41:34 -0400

    Extracted series title and ordered them

--- src/components/Series/Series.js
+++ src/components/Series/Series.js
@@ -4,25 +4,30 @@ import data from "../../feed/sample.json";
 
 function Series() {
   let series = []
+  let seriesTitle = [];
   data.entries.map((item) => (
     item.releaseYear >= 2010 && item.programType === 'series'
     ?
-    series.push(item)
+    // series.push(item)
+    seriesTitle.push(item.title)
     :
     null
   ))
+  seriesTitle = seriesTitle.sort().slice(0, 21);
+  // Extract
   // Order by item.title in series
   return (
     <div className={styles.Series} data-testid="Series">
       <div class="container">
-        <div class="row justify-content-md-center">
+        {/* <div class="row justify-content-md-center">
           {series.slice(0, 21).map((item, i) => (
             <div className={styles.card}>
               <img class="card-img-top" src={item.images["Poster Art"].url} alt="Series Card" />
               <p>{item.title}</p>
             </div>
           ))}
-        </div>
+        </div> */}
+        <div>{seriesTitle.map((title) => title)}</div>
       </div>
     </div>
   )
