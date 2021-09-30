91b9832344517b690dc32ec9cb68468d8bae710e
Lawrence Dovin <lawrencedovin@gmail.com>
2021-09-30 10:24:14 -0400

    temp code

--- src/components/Series/Series.js
+++ src/components/Series/Series.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from './Series.module.scss';
 import data from "../../feed/sample.json";
 
 function Series() {
+
+  const [state, setState] = useState(data);
+
+  useEffect(() => {
+    console.log('hello')
+  }, [state])
+
   let series = []
   let seriesTitle = [];
   
   // Extracts item titles to array
-  data.entries.map((item) => (
+  state.entries.map((item) => (
     item.releaseYear >= 2010 && item.programType === 'series'
     ?
     seriesTitle.push(item.title)
@@ -20,24 +27,32 @@ function Series() {
 
   // Loops through seriesTitle in order if the seriesTitle matches in json data then pushes it to series array.
   for(let i = 0; i < seriesTitle.length; i++) {
-    data.entries.map((item) => (
+    state.entries.map((item) => (
       item.title === seriesTitle[i] ? series.push(item) : null
     ))
   }
 
   return (
-    <div className={styles.Series} data-testid="Series">
-      <div class="container">
-        <div class="row justify-content-md-center">
-          {series.map((item, i) => (
-            <div className={styles.card}>
-              <img class="card-img-top" src={item.images["Poster Art"].url} alt="Series Card" />
-              <p>{item.title}</p>
+      <>
+      {
+        state 
+        ? 
+        <div className={styles.Series} data-testid="Series">
+            <div class="container">
+              <div class="row justify-content-md-center">
+                {series.map((item) => (
+                  <div className={styles.card}>
+                    <img class="card-img-top" src={item.images["Poster Art"].url} alt="Series Card" />
+                    <p>{item.title}</p>
+                  </div>
+                ))}
+              </div>
             </div>
-          ))}
         </div>
-      </div>
-    </div>
+        : 
+        <p>Loading...</p>
+      }
+      </>
   )
 }
 
